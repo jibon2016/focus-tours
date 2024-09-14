c9c57325e0e2bb81d1e63c8de44f2c8f915e6e41
jibon2016 <sdjibon2016@gmail.com>
2024-09-14 21:58:34 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Master from './components/layout/Master'
+
+function App() {
+  return (
+    <Master>
+      <h1>Hello World</h1>
+    </Master>
+  )
+}
+
+export default App
---
+++ src/components/layout/Master.jsx
@@ -0,0 +1,7 @@
+export default function Master({children}){
+  return (
+    <main className="h-screen bg-green-100 w-full">
+      {children}
+    </main>
+  )
+}
