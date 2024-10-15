fc4ef53c79f2b5d9384b7fb84dd2dac6d22a9606
jibon2016 <sdjibon2016@gmail.com>
2024-10-16 00:20:06 +0600

    Slider Working

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
+import Slider from 'react-slick'
 
 export default function Services() {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        autoplay: true,
+        autoplaySpeed: 2000
+    };    
     return (
         <div className='relative w-full'>
-            <div className='absolute bg-white top-0 md:-top-[290px] py-10 px-10'>
+            <div className=' top-0 md:-top-[290px] py-10 px-10'>
                 <h2 className='text-center text-3xl md:text-4xl py-2 font-madimi-one capitalize'>Our Services</h2>
-                <div className="py-4">
-                    <div className='grid grid-cols-2 md:grid-cols-4 gap-4 text-justify'>
+                <div className="py-4 text-justify slider-container">
+                    <Slider {...settings}>
                         <div className="bg-red-400 py-2 px-4"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum qui dolor tempora laudantium corporis ipsam similique, minima fuga cupiditate dolores quae provident dolorum! Impedit enim, veniam velit eaque odio autem.</p></div>
                         <div className="bg-red-400 py-2 px-4"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum qui dolor tempora laudantium corporis ipsam similique, minima fuga cupiditate dolores quae provident dolorum! Impedit enim, veniam velit eaque odio autem.</p></div>
                         <div className="bg-red-400 py-2 px-4"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum qui dolor tempora laudantium corporis ipsam similique, minima fuga cupiditate dolores quae provident dolorum! Impedit enim, veniam velit eaque odio autem.</p></div>
                         <div className="bg-red-400 py-2 px-4"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum qui dolor tempora laudantium corporis ipsam similique, minima fuga cupiditate dolores quae provident dolorum! Impedit enim, veniam velit eaque odio autem.</p></div>
-                    </div>
+                        <div className="bg-red-400 py-2 px-4"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum qui dolor tempora laudantium corporis ipsam similique, minima fuga cupiditate dolores quae provident dolorum! Impedit enim, veniam velit eaque odio autem.</p></div>
+                    </Slider>
                 </div>
             </div>
         </div>
