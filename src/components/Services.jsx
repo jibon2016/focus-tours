aaecda030ee74b055f9dae0c9f4560ef6e5a882a
Jibon <sdjibon2016@gmail.com>
2024-10-16 11:17:02 +0600

    Fix slider Error

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -13,15 +13,15 @@ export default function Services() {
     };    
     return (
         <div className='relative w-full'>
-            <div className=' top-0 md:-top-[290px] py-10 px-10'>
+            <div className='bg-white -mt-[260px] top-0 md:-top-[290px] py-10 px-10'>
                 <h2 className='text-center text-3xl md:text-4xl py-2 font-madimi-one capitalize'>Our Services</h2>
                 <div className="py-4 text-justify slider-container">
                     <Slider {...settings}>
-                        <div className="bg-red-400 py-2 px-4"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum qui dolor tempora laudantium corporis ipsam similique, minima fuga cupiditate dolores quae provident dolorum! Impedit enim, veniam velit eaque odio autem.</p></div>
-                        <div className="bg-red-400 py-2 px-4"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum qui dolor tempora laudantium corporis ipsam similique, minima fuga cupiditate dolores quae provident dolorum! Impedit enim, veniam velit eaque odio autem.</p></div>
-                        <div className="bg-red-400 py-2 px-4"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum qui dolor tempora laudantium corporis ipsam similique, minima fuga cupiditate dolores quae provident dolorum! Impedit enim, veniam velit eaque odio autem.</p></div>
-                        <div className="bg-red-400 py-2 px-4"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum qui dolor tempora laudantium corporis ipsam similique, minima fuga cupiditate dolores quae provident dolorum! Impedit enim, veniam velit eaque odio autem.</p></div>
-                        <div className="bg-red-400 py-2 px-4"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum qui dolor tempora laudantium corporis ipsam similique, minima fuga cupiditate dolores quae provident dolorum! Impedit enim, veniam velit eaque odio autem.</p></div>
+                        <div className="px-1"><div className='bg-gray-200 rounded-md  border py-4 px-4'><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum qui dolor tempora laudantium corporis ipsam similique, minima fuga cupiditate dolores quae provident dolorum! Impedit enim, veniam velit eaque odio autem.</p></div></div>
+                        <div className="px-1"><div className='bg-gray-200 rounded-md  border py-4 px-4'><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum qui dolor tempora laudantium corporis ipsam similique, minima fuga cupiditate dolores quae provident dolorum! Impedit enim, veniam velit eaque odio autem.</p></div></div>
+                        <div className="px-1"><div className='bg-gray-200 rounded-md  border py-4 px-4'><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum qui dolor tempora laudantium corporis ipsam similique, minima fuga cupiditate dolores quae provident dolorum! Impedit enim, veniam velit eaque odio autem.</p></div></div>
+                        <div className="px-1"><div className='bg-gray-200 rounded-md  border py-4 px-4'><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum qui dolor tempora laudantium corporis ipsam similique, minima fuga cupiditate dolores quae provident dolorum! Impedit enim, veniam velit eaque odio autem.</p></div></div>
+                        <div className="px-1"><div className='bg-gray-200 rounded-md  border py-4 px-4'><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum qui dolor tempora laudantium corporis ipsam similique, minima fuga cupiditate dolores quae provident dolorum! Impedit enim, veniam velit eaque odio autem.</p></div></div>
                     </Slider>
                 </div>
             </div>
