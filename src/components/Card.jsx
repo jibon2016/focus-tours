b0547a9792cbfa543eff52f4d4c0e9126fdd9150
jibon2016 <sdjibon2016@gmail.com>
2024-10-27 23:13:29 +0600

    Component Pass in Props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import PassportAnimation from './PassportAnimation'
 
-export default function Card() {
+export default function Card({Animation}) {
   return (
     <div className="px-2">
         <div className='bg-gray-50 rounded-md border-gray-300 border py-4 px-4'>
-          <PassportAnimation></PassportAnimation>
+          {Animation}
           <h4 className='text-center text-2xl font-semibold pb-2'>Heading</h4>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum qui dolor tempora laudantium corporis ipsam similique, minima fuga cupiditate dolores quae provident dolorum! Impedit enim, veniam velit eaque odio autem.</p>
         </div>
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Slider from 'react-slick'
 import Card from './Card';
+import PassportAnimation from './PassportAnimation';
+import { BsPassFill } from 'react-icons/bs';
 
 
 
@@ -25,13 +27,14 @@ export default function Services() {
             },
         ],
     };    
+    const PassportComponent = <PassportAnimation></PassportAnimation>
     return (
         <div className='relative w-full'>
             <div className='bg-white -mt-[310px] top-0 md:-top-[290px] py-10 px-10'>
                 <h2 className='text-center text-3xl md:text-4xl py-2 font-madimi-one capitalize'>Our Services</h2>
                 <div className="py-4 text-justify slider-container">
                     <Slider {...settings}>
-                        <Card/>
+                        <Card Animation={PassportComponent} />
                         <Card/>
                         <Card/>
                         <Card/>
